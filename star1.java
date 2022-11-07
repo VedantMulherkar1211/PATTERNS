a0cf11759297015a27b4eeb12b2b69cbe47c3442
vedant <72485535+VedantMulherkar1211@users.noreply.github.com>
2022-11-07 20:48:24 +0530

    Add files via upload

---
+++ star1.java
@@ -0,0 +1,22 @@
+package pattern1;
+
+public class star1 
+{
+
+	public static void main(String[] args)
+	{
+		for(int i=0;i<=4;i++)
+		{
+			for(int j=0;j<=4;j++)
+			{
+				System.out.print("*");
+				System.out.print("  ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
+
+
